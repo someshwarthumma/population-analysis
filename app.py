9562c06e42424fa78a4863be95b45108a6705457
Someshwar Thumma <somesh.sandeep619@msitprogram.net>
2023-04-05 18:06:47 +0530

    Years Filter Updated

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from werkzeug.datastructures import MultiDict
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/data',methods=['GET'])
+def data():
+   cols = ['country','year','population','population_density','population_growth_rate']
+   data = pd.read_csv('./static/data/population.csv',skipinitialspace=True)
+   data.columns = cols
+   data['population'] = data['population'].str.strip()
+   data['population_density'] = data['population_density'].str.strip()
+   data['population_growth_rate'] = data['population_growth_rate'].str.strip()
+   # data['year'] = data['year'].astype('int')
+   # data['population'] = data['population'].astype('int')
+   # data['population_density'] = data['population_density'].astype('float')
+   # data['population_growth_rate'] = data['population_growth_rate'].astype('float')
+   url_args = MultiDict(request.args)
+   # if(len(url_args.getlist('year')) != 0):
+   #    data = data[data['year'].isin([int(s) for s in url_args.getlist('year')])]
+   return data.to_json(orient="records")
+
+@app.route('/years',methods=['GET'])
+def years():
+   cols = ['country','year','population','population_density','population_growth_rate']
+   data = pd.read_csv('./static/data/population.csv',skipinitialspace=True)
+   data.columns = cols
+   return data['year'].unique().tolist()
+
 if __name__ == '__main__':
    app.run()
---
+++ static/js/common.js
@@ -0,0 +1,41 @@
+let global_data = {}
+
+function get_data() {
+    $.get('./data').done(function(data){
+        _.assign(global_data, prepare_data(perform_typeCast(JSON.parse(data))))
+        render_page()
+    }).fail(function() {
+        
+    })
+}
+
+function perform_typeCast(data){
+    _.map(data, d=>{
+        d['year'] = parseInt(d['year'])
+        d['population'] = numeral(d['population']).value() //population_growth_rate
+        d['population_density'] = parseInt(d['population_density']) 
+        d['population_growth_rate'] = parseFloat(d['population_growth_rate']) 
+    })
+    return data
+}
+
+function prepare_data(data) {
+    let results = {}
+    let years =  {'years':Object.keys(_.groupBy(data,'year')).sort().reverse()}
+    _.assign(results, {'filters':years})
+
+
+    return results 
+}
+
+function render_page() {
+    _.map(global_data.filters.years, d=>{
+        $('#year_select').append($("<option></option>").attr("value",d).text(d))
+    })
+    // debugger
+    
+}
+
+$(document).ready(function () {
+    get_data()
+  });
